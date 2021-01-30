01d1f006b085c72245b02eee8233c502d0d7e66e
guokenlin1 <59401241+guokenlin1@users.noreply.github.com>
2021-01-30 14:51:49 -0500

    server setup

---
+++ server.js
@@ -0,0 +1,27 @@
+'use strict';//strict mode
+const express = require("express");
+const body = require("body-parser");
+const app = express();
+
+app.set('view engine', 'ejs');
+app.use(express.static(__dirname + "/public"));
+app.use(body.urlencoded({ extended: true }));
+
+app.get("/", function(req, res){
+    res.render("home");
+})
+
+app.post("genratePDF", function(req, res){
+
+    
+
+})
+
+// Take any port number of your choice which 
+// is not taken by any other process 
+app.listen(3000, function (error) {
+    if (error) throw error
+    console.log("Server created Successfully on PORT 3000")
+
+})
+
